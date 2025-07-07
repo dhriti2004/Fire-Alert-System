7acf474235d7241862ee751b68aac2b23fc5e073
dhriti2004 <dhritisharma952@gmail.com>
2025-07-07 21:54:54 +0530

    Update sketch_jul5a.ino

--- VIT_25/sketch_jul5a.ino
+++ VIT_25/sketch_jul5a.ino
@@ -3,34 +3,34 @@
 #define buzzer 4
 #define led 5
 
-int gasThreshold = 300;    // Adjust as needed
-int tempThreshold = 51;    // Celsius for LM35
+int gasThreshold = 300;    
+int tempThreshold = 50;    
 
 void setup() {
   Serial.begin(9600);
   pinMode(buzzer, OUTPUT);
   pinMode(led, OUTPUT);
-  digitalWrite(led, LOW);    // LED off by default
+  digitalWrite(led, LOW);   
 }
 
 void loop() {
   int gasValue = analogRead(gasSensor);
   int tempValue = analogRead(tempSensor);
-  float voltage = tempValue * (5000.0 / 1024.0); // in millivolts
-  float temperature = voltage / 10; // 10mV per °C
+  float voltage = tempValue * (5000.0 / 1024.0); 
+  float temperature = voltage / 10; 
 
 
   Serial.print("Gas: "); Serial.print(gasValue);
   Serial.print(" | Temp: "); Serial.println(temperature);
 
   if (gasValue > gasThreshold || temperature > tempThreshold) {
-    digitalWrite(buzzer, HIGH);  // Buzzer on
-    digitalWrite(led, HIGH);     // LED on
+    digitalWrite(buzzer, HIGH);  
+    digitalWrite(led, HIGH);     
     delay(300);
     digitalWrite(buzzer, LOW);
     delay(300);
   } else {
-    digitalWrite(buzzer, LOW);   // Buzzer off
+    digitalWrite(buzzer, LOW);   
     digitalWrite(led, LOW);      // LED off
   }
   delay(1000);
